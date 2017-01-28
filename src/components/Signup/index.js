bc1d2e6de0d591b5b5af542718702e37a5ccb694
Rishabh Mishra <mishrarishabh865@gmail.com>
2017-01-28 18:39:39 +0530

    Signup form Created

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,11 +1,19 @@
 import React , { Component } from 'react'
-import { View, StyleSheet, Alert } from 'react-native'
+import { View, StyleSheet, Alert,  AsyncStorage, } from 'react-native'
 import { Container, Content, Title, Text, Header, InputGroup, Input, Icon, Button } from 'native-base'
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 import bcrypt from 'react-native-bcrypt'
 import uuid from 'uuid'
+import RSAKey from 'react-native-rsa'
+import {RSA_SECRET} from 'react-native-dotenv'
+
+const rsa = new RSAKey()
+const bits = 1024;
+const exponent = '10001'; // must be a string
+var r = rsa.generate(bits, RSA_SECRET);
 export default class Signup extends Component {
   submitButton () {
+    
     const salt = bcrypt.genSaltSync(10)
     if (!this.state || !this.state.fullname || !this.state.password || !this.state.cpassword) {
       Alert.alert('Please Fill all the Details')
@@ -15,14 +23,33 @@ export default class Signup extends Component {
       Alert.alert('Enter Valid Mobile Number')
     }else {
       const userid = uuid.v1()
+      const publicKey = rsa.getPublicString(); // return json encoded string
+      const privateKey = rsa.getPrivateString(); // return json encoded string
       const hash = bcrypt.hashSync(this.state.password, salt)
       if (bcrypt.compareSync(this.state.cpassword, hash)) {
-        console.log(this.state, hash,userid)
+        const obj = {
+          userid,
+          publicKey,
+          privateKey,
+          name: this.state.fullname,
+          password: hash,
+          mobile: this.state.mobile
+        }
+       this._saveData(obj).done();
       }else {
         Alert.alert('Password does not match')
       }
     }
   }
+  _saveData = async (obj) => {
+    try {
+      await AsyncStorage.setItem('@igotu:'+this.state.mobile,JSON.stringify(obj));
+    } catch (error) {
+     Alert.alert("Please Try Again")
+     console.log(error)
+    }
+  }
+
   render () {
     return (
       <Container>
